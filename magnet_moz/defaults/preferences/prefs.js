296756ef842cab71da42098356eda2b1bea02431
yuin <stopcloud@gmail.com>
2011-03-14 10:09:08 -0700

    create monster_vis

--- magnet_moz/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.magnet.boolpref", false);
-pref("extensions.magnet.intpref", 0);
-pref("extensions.magnet.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.app@detourlab.com.description", "chrome://magnet/locale/overlay.properties");
