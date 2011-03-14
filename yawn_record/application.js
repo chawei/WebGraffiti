b7408a3d0c14c26417e3a5049923ef27a7e5989a
yuin <stopcloud@gmail.com>
2011-03-13 19:54:23 -0700

    add magnet visualization

---
+++ yawn_record/application.js
@@ -0,0 +1,257 @@
+google.load('search', '1');
+var currentKeywords = "";
+
+function webSearchComplete(searcher) {
+	if (searcher.results && searcher.results.length > 0) {
+    // Grab our content div, clear it.
+    var contentDiv = document.getElementById('contentWeb');
+    contentDiv.innerHTML = '';
+
+    var results = searcher.results;
+    for (var i = 0; i < results.length; i++) {
+     	var result = results[i];
+			
+			var result_div = $('<div class="websearch_div"></div>');
+			$('#contentWeb').append(result_div);
+			result_div.hide().delay(Math.random()*3000+200).fadeIn(1000);
+			result_div.append('<div class="websearch_title"><a href="'+result.url+'" target="_blank">'+result.title+'</a></div>');
+			result_div.append('<div class="websearch_content">'+result.content+'</div>');
+		}
+	}
+	else {
+		$('#result_doc_container').addClass('hidden');
+		if( $('#result_img_container').hasClass('hidden') )
+			$('#content_desc').html('Oops! There is no search results for keyword: <b>'+currentKeywords+'</b>');
+	}
+}
+function imageSearchComplete(searcher) {
+
+  // Check that we got results
+  if (searcher.results && searcher.results.length > 0) {
+    // Grab our content div, clear it.
+    var contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = '';
+
+    var results = searcher.results;
+    for (var i = 0; i < results.length; i++) {
+     	var result = results[i];
+
+			var result_div = $('<div class="result_div"></div>');
+			$('#content').append(result_div);
+			result_div.hide().delay(Math.random()*3000+200).fadeIn(1000);
+
+			var result_image = $('<img class="result_img"/>');
+			result_image.attr('src',result.tbUrl);
+			result_div.append(result_image);
+			result_image[0].onload = createWGResultImage;
+			
+			result_image.wrap('<a href="'+result.url+'" target="_blank"></a>');
+    }
+  }
+	else {
+		$('#result_img_container').addClass('hidden');
+		if( $('#result_doc_container').hasClass('hidden') )
+			$('#content_desc').html('Oops! There is no search results for keyword: <b>'+currentKeywords+'</b>');
+	}	
+}
+function createWGResultImage() {
+	var w = 100/this.height*this.width;
+	var rimage = new WGResultImage(this.parentNode,w,100);
+}
+
+var fakeKeywords = ["geek movie","revamp roy"];
+var fakeINT = 0;
+
+function submitSearch() {
+	var keywords = trim( $('#input').val() );
+	if(keywords.length==0)
+		return;
+	else if(keywords.length>1)
+		keywords = messedUp(keywords);
+	
+	
+	keywords = fakeKeywords[fakeINT];
+	fakeINT++;
+	
+	// $('#img_container').hide();
+	$('#img_container').css('width','900px').css('margin-top','10px');
+	$('#img_container img').css('width','150px').css('float','left').css('padding-top','30px');
+	$('#main_container').css('padding-left','140px');
+	
+	var content_desc = document.getElementById('content_desc');
+  content_desc.innerHTML = 'Search Results for: <b>'+keywords+'</b>';
+
+	// if($('#content_border').length==0) {
+	// 				$('#content_desc').after('<div id="content_border"></div>');
+	// 				var border = new WGBorder( $('#content_border') );
+	// 			}
+
+	currentKeywords = keywords;
+	
+  var imageSearch = new google.search.ImageSearch();
+  imageSearch.setRestriction(google.search.ImageSearch.RESTRICT_IMAGESIZE,
+                             google.search.ImageSearch.IMAGESIZE_MEDIUM);
+	var options = new GsearcherOptions();
+	var control = new GSearchControl();
+  control.setResultSetSize(GSearch.LARGE_RESULTSET);
+	control.addSearcher(imageSearch, options);
+ 	imageSearch.setSearchCompleteCallback(this, imageSearchComplete, [imageSearch]);
+  imageSearch.execute('"'+keywords+'"');
+
+	var webSearch = new google.search.WebSearch();
+  var searchControl = new google.search.SearchControl();
+  searchControl.addSearcher(webSearch);
+  //      searchControl.addSearcher(new google.search.VideoSearch());
+  //      searchControl.addSearcher(new google.search.BlogSearch());
+  //      searchControl.addSearcher(new google.search.NewsSearch());
+  // searchControl.addSearcher(imageSearch);
+  //searchControl.draw(document.getElementById("content2"));
+	webSearch.setSearchCompleteCallback(this, webSearchComplete, [webSearch]);
+  webSearch.execute('"'+keywords+'"');
+	
+	$('#dynamic_textfield')[0].innerHTML = '';
+	$("#input").attr("value","");
+}
+
+function messedUp(keywords) {
+	var messedUpKeywords = '';
+	var sIdx = Math.floor( Math.random()*5 );
+	switch(sIdx) {
+		case 0:// swap 2 letters
+			var c1,c2;
+			c1 = Math.floor( Math.random()*keywords.length );
+			if(c1<keywords.length-1)
+				c2 = c1+1;
+			else if(c1>0)
+				c2 = c1-1;
+			for(var i=0; i<keywords.length; i++) {
+				if(i==c1)
+					messedUpKeywords+=keywords[c2];
+				else if(i==c2)
+					messedUpKeywords+=keywords[c1];
+				else
+					messedUpKeywords+=keywords[i];
+			}
+			break;
+		// case 1:// anagram
+		// 					messedUpKeywords = anagram(keywords);
+		// 					break;
+		case 1:// miss 1 letter
+			var idx = Math.floor( Math.random()*keywords.length );
+			for(var i=0; i<keywords.length; i++) {
+				if(i!=idx)
+					messedUpKeywords+=keywords[i];
+			}
+			break;
+		case 2:
+			messedUpKeywords = keywords;
+			break;
+		case 3:// swap 2 letters
+			var c1,c2;
+			c1 = Math.floor( Math.random()*keywords.length );
+			if(c1<keywords.length-1)
+				c2 = c1+1;
+			else if(c1>0)
+				c2 = c1-1;
+			for(var i=0; i<keywords.length; i++) {
+				if(i==c1)
+					messedUpKeywords+=keywords[c2];
+				else if(i==c2)
+					messedUpKeywords+=keywords[c1];
+				else
+					messedUpKeywords+=keywords[i];
+			}
+			break;
+		// case 1:// anagram
+		// 					messedUpKeywords = anagram(keywords);
+		// 					break;
+		case 4:// miss 1 letter
+			var idx = Math.floor( Math.random()*keywords.length );
+			for(var i=0; i<keywords.length; i++) {
+				if(i!=idx)
+					messedUpKeywords+=keywords[i];
+			}
+			break;
+	}
+	return trim(messedUpKeywords);
+}
+
+function anagram(beforeStr) {
+	beforeStr = beforeStr.toLowerCase();
+	
+	aryText = new Array(beforeStr.length);
+	len = aryText.length;
+
+	for (i = 0; i < len; i++) {
+		aryText[i] = beforeStr.charAt(i);
+	}
+
+	newnumber = "";
+	temp = "";
+	i = 0;
+
+	for (i = 1; i < len; i++) {
+		newnumber = (Math.random() * len);
+		newnumber = parseInt(newnumber, 10);
+		temp = aryText[i];
+		aryText[i] = aryText[newnumber];
+		aryText[newnumber] = temp;
+	}
+
+	NewName = "";
+	NewName = aryText.join("");
+
+	return NewName;
+}
+function trim(s){
+  return ( s || '' ).replace( /^\s+|\s+$/g, '' ); 
+}
+
+	var styleElement;
+	var canvasElement;
+	var webGraffiti = null;
+
+	$(document).ready(function() {
+	  
+		// if ($.browser.webkit) {
+		//   $('body').removeClass('hidden');
+		// } else {
+		// 	alert("This project only works in Chrome, and Safari!");
+		// 	$('#main_container').html('Please Launch this Project in <b>Google Chrome</b> or <b>Safari</b>!');
+		// 	$('body').removeClass('hidden');
+		// 	return;
+		// }
+		
+		$('body').removeClass('hidden');
+		
+		if(webGraffiti==null) {
+			webGraffiti = new WGGoogle();
+			webGraffiti.init();
+			$('center').fadeIn();
+			
+			$('#search_form').submit(function(e){
+				e.preventDefault();
+				$('#content').html('');
+				$('#contentWeb').html('');
+				
+				if( $('#result_doc_container').hasClass('hidden') )
+					$('#result_doc_container').removeClass('hidden');
+				if( $('#result_img_container').hasClass('hidden') )
+					$('#result_img_container').removeClass('hidden');
+
+				$('#footnote').hide().delay(5000).fadeIn(1000);
+				submitSearch();
+			});
+			
+			$('#input_luck').hover(
+				function(){
+					this.value = "Out of Service...";
+				},
+				function(){
+					this.value = "I'm Feeling Lucky";
+				}
+			).click(function(e){
+				e.preventDefault();
+			});
+		}
+	});
---
+++ yawn_record/wg_google.js
@@ -0,0 +1,196 @@
+function WGGoogle() {
+  
+	var flagTxtfield, flagBtn, flagClock;
+	var wgTxtfieldSearch;
+	var timerINT;
+	var wgBtnSearch;
+	var wgBtnLucky;
+	var timerCount=-1;
+	var sounds = new Array('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16');
+	var soundsSize   = [ 1.0,1.0,1.0,1.0,1.0,1.2,1.0,1.2,1.0,1.3,1.3,1.0,1.2,1.1,1.0,1.0];
+	var soundsPeriod = [  10,  9,  9, 12, 17, 13, 12, 12, 12, 12, 10,  8,  8,  8, 10, 10];
+	
+	var currentSoundIdx = 0;
+	var sound;
+	
+	var checkTimer = 0;
+	
+	var metaKey = false;	
+	var searchTextField;
+	
+	var currentYawnPeriod = 0;
+	
+	function initAudio() {
+		for(var i=0; i<sounds.length; i++) {
+			var sound = document.createElement('embed');
+			sound.setAttribute("loop","false");
+		  sound.setAttribute("type","audio/x-wav");
+		  sound.setAttribute("hidden","true");
+			sound.setAttribute("id","yawn_audio_"+sounds[i]);
+			sound.setAttribute("autostart","false");
+			sound.setAttribute('src', "assets/yawn/"+sounds[i]+".mov");
+		  document.body.appendChild(sound);
+		}
+		sounds.sort( randOrd );
+//		console.log(sounds);
+	}
+	
+  this.init = function() {
+		
+		flagTxtfield = true;
+		flagBtn = false;
+		flagClock = false;
+		
+		// init sounds
+		initAudio();
+		
+		// draw textfield
+		var input = document.getElementById('input_div');
+		input.style.position = 'relative';
+		input.childNodes[0].style.zIndex = 2;
+		wgTxtfieldSearch = new WGTextfield(input, flagTxtfield);
+		searchTextField = YawnTextField.create('div#input_div input', wgTxtfieldSearch);
+		
+		var buttons = $('.lsbb input');
+		buttons.css('font-size','14px');
+		
+		var button1 = $(buttons[0]);
+		var button2 = $(buttons[1]);
+		var btn0_left = button1.position().left;
+		var btn0_top = button1.position().top+2;
+		var btn1_left = button2.position().left+7;
+		var btn1_top = button2.position().top+2;	
+		
+		wgBtnSearch = new WGButtonForYawn( button1, btn0_left, btn0_top, flagBtn);
+		wgBtnLucky = new WGButtonForYawn( button2, btn1_left, btn1_top, flagBtn);
+		
+		$('div#input_div').append('<div id="dynamic_textfield"></div>');
+		$('div#input_div input').css('opacity', 1.0);
+		$('#dynamic_textfield').hide();
+
+//		setTimeout( activateYawn,3000);
+
+		timerINT = setInterval(timerHandler,30);
+		
+		// new hyperlink
+		$('.result').each(function(index) {
+			var hl = new WGHyperLink($(this));
+		});
+		
+  }
+  
+ 	function timerHandler() {
+		if(wgTxtfieldSearch.isActive()==false) {
+			checkTimer++;
+			if(Math.random()<0.004 || checkTimer>140*(Math.random()+1))
+				activateYawn();
+		}
+		else if(timerCount>=currentYawnPeriod*8) {
+			wgBtnSearch.resetPosition();
+			wgBtnLucky.resetPosition();
+			timerCount=-1;
+			checkTimer=0;
+		}
+		else if(timerCount!=-1)	{
+			timerCount++;
+		}
+	}
+	
+	function randOrd(){
+		return (Math.round(Math.random())-0.5);
+	}
+	
+	function activateYawn() {
+		if(currentSoundIdx<sounds.length-1)
+			currentSoundIdx++;
+		else
+			currentSoundIdx = 0;
+		
+		var soundId = "yawn_audio_"+sounds[currentSoundIdx];
+		document.getElementById(soundId).Play();
+		
+		var idx = parseInt(sounds[currentSoundIdx])-1;
+		var yawnSize = soundsSize[idx];
+		var yawnPeriod = soundsPeriod[idx];
+		currentYawnPeriod = yawnPeriod;
+		
+		wgTxtfieldSearch.activeYawn(yawnSize,yawnPeriod);
+		wgBtnSearch.setTargetPosition( -50+Math.random()*30, Math.random()*40*yawnSize+40);
+		wgBtnLucky.setTargetPosition( 50-30*Math.random(), Math.random()*40*yawnSize+40);
+		timerCount = 0;
+		checkTimer = 0;
+	}	
+}
+
+var YawnTextField = {
+  create: function(selector, wgTxtfieldSearch) {
+    var yawnTextField= $(selector);
+    
+    function refreshDynamicField() {
+      var value = yawnTextField.val();
+      var formatValue = '';
+      for(var i=0; i<value.length; i++) {	 	
+        var id = "dt_"+i;
+        if(value[i]==' ') {
+          formatValue += '<span id="'+id+'">&nbsp;&nbsp;</span>';
+        } else {
+          formatValue += '<span id="'+id+'">' + value[i] + '</span>';
+        }
+      }
+      $('#dynamic_textfield').html(formatValue);
+    }
+
+    yawnTextField.keypress(function(e){      
+		  var value = $(this).val();
+			var newChar = String.fromCharCode(e.which);		
+
+		  var range = $(this).getSelection();
+		  var insertHTML = '';
+
+			if (e.which == 32) {
+				insertHTML = '<span>&nbsp;</span>';
+			} else {
+				insertHTML = '<span>' + newChar + '</span>';
+			}
+			
+			var insertIndex = range.start-1;
+			if (insertIndex < 0) {
+			  $('#dynamic_textfield').append(insertHTML);
+			} else {
+			  $(insertHTML).insertAfter('#dynamic_textfield span:eq('+insertIndex+')'); 
+			}
+		});
+
+		yawnTextField.keyup(function(e){
+		  if (metaKey) {
+        refreshDynamicField();
+      }
+	  });
+
+		yawnTextField.keydown(function(e){
+      if (e.metaKey) {
+			  metaKey = true;
+      } else { 
+        metaKey = false;
+      }
+
+      var range = yawnTextField.getSelection();
+			if (e.which == 8 && wgTxtfieldSearch.isActive()) {
+			  if (yawnTextField.val().length == range.length) {
+          $('#dynamic_textfield span').remove();
+        } else {
+          if (range.length == 0) {
+            $('#dynamic_textfield span:eq('+(range.start-1)+')').remove();
+          } else {
+    			  $('#dynamic_textfield span').slice(range.start, range.end).remove();
+  			  }
+			  }
+
+			}	else if (!wgTxtfieldSearch.isActive()) {
+			  refreshDynamicField();
+			}
+		});
+
+		return yawnTextField;
+  }
+}
